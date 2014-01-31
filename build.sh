94e232631f711a2a9011c3694291a308ab32be35
funkygao <funky.gao@gmail.com>
2014-01-31 15:44:19 +0800

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
-cd $(dirname $0)
+cd $(dirname $0)/cmd/fproxyd
 ID=$(git rev-parse HEAD | cut -c1-7)
+
 go build -v -ldflags "-X main.BuildID $ID"
 
+#---------
+# show ver
+#---------
+./fproxyd -version
