636c651f5da4a3a7b6358126a018c6eeacec87ac
funkygao <funky.gao@gmail.com>
2014-01-31 15:37:04 +0800

    engine

--- engine/engine.go
+++ engine/engine.go
@@ -1,1 +1,21 @@
 package engine
+
+import (
+//conf "github.com/daviddengcn/go-ljson-conf"
+)
+
+type Engine struct {
+}
+
+func NewEngine() (this *Engine) {
+	this = new(Engine)
+	return
+}
+
+func (this *Engine) LoadConfigFile(fn string) *Engine {
+	return this
+}
+
+func (this *Engine) ServeForever() {
+
+}
