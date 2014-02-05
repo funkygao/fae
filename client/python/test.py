449f0103464e3313461d13aa56a5da05843e2777
funkygao <funky.gao@gmail.com>
2014-02-05 15:34:56 +0800

    clean result

--- client/python/test.py
+++ client/python/test.py
@@ -10,29 +10,50 @@ sys.path.append('../../servant/gen-py')
 sys.path.append('/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
 from thrift.transport import TSocket
 from thrift.protocol import TBinaryProtocol
+from thrift.transport.TTransport import TTransportException
+from thrift.Thrift import TApplicationException
 from fun.rpc import FunServant
+from fun.rpc.ttypes import TCacheMissed
 
 t1 = datetime.datetime.now()
 sock = TSocket.TSocket('localhost', 9001)
-sock.open()
+try:
+    sock.open()
+except TTransportException, e:
+    print e
+    sys.exit(1)
 protocol = TBinaryProtocol.TBinaryProtocol(sock)
 
 client = FunServant.Client(protocol)
 ctx = FunServant.req_ctx(caller='POST+/facebook/getPaymentRequestId/+34ca2cf6')
 
+def elapsed():
+    global t1
+    delta = datetime.datetime.now() - t1
+    t1 = datetime.datetime.now()
+    return '(' + str(delta.microseconds) + ' us)'
+
 # ping
 #=====
 r = client.ping(ctx)
 delta = datetime.datetime.now() - t1
-print '[Client] received from rpc server:', r, delta.microseconds, 'us'
+print '[Client] ping received:', r, elapsed()
 
-print client.mc_set(ctx, 'hello', 'world 世界', 120)
-print client.mc_get(ctx, 'hello')
+# mc
+#=====
+print '[Client] mc_set received:', client.mc_set(ctx, 'hello', 'world 世界', 120), elapsed()
+print '[Client] mc_get received:', client.mc_get(ctx, 'hello'), elapsed()
 
 try:
-    print 'hello-non-exist ->', client.mc_get(ctx, 'hello-non-exist')
-except Exception, e:
+    print '[Client] mc_get hello-non-exist ->', client.mc_get(ctx, 'hello-non-exist'), elapsed()
+except TCacheMissed:
+    print 'cache missed'
+except TApplicationException, e:
     print e
+except Exception, e:
+    print e, type(e)
 
-print client.lc_set(ctx, 'lc_test_hello', 'abcdefg')
-print client.lc_get(ctx, 'lc_test_hello')
+# lc
+#=====
+print '[Client] lc_set received:', client.lc_set(ctx, 'lc_test_hello', 'abcdefg'), elapsed()
+print '[Client] lc_get received:', client.lc_get(ctx, 'lc_test_hello'), elapsed()
