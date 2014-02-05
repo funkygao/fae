4725d630fd2e2b00f1ecc8d143e40edbd304ad9d
funkygao <funky.gao@gmail.com>
2014-02-05 15:35:21 +0800

    log err when memcache fails

--- servant/mc.go
+++ servant/mc.go
@@ -4,6 +4,7 @@ MCache key:string, value:[]byte.
 package servant
 
 import (
+	log "code.google.com/p/log4go"
 	"git.apache.org/thrift.git/lib/go/thrift"
 	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
 	"github.com/funkygao/fae/servant/memcache"
@@ -11,10 +12,12 @@ import (
 
 func (this *FunServantImpl) McSet(ctx *rpc.ReqCtx, key string, value []byte,
 	expiration int32) (r bool, intError error) {
-	err := this.mc.Set(&memcache.Item{Key: key, Value: value,
+	intError = this.mc.Set(&memcache.Item{Key: key, Value: value,
 		Expiration: expiration})
-	if err == nil {
+	if intError == nil {
 		r = true
+	} else {
+		log.Error(intError)
 	}
 
 	return
@@ -30,6 +33,9 @@ func (this *FunServantImpl) McGet(ctx *rpc.ReqCtx, key string) (r []byte,
 		// cache miss
 		miss = rpc.NewTCacheMissed()
 		miss.Message = thrift.StringPtr(err.Error()) // optional
+	} else {
+		intError = err
+		log.Error(err)
 	}
 
 	return
@@ -37,10 +43,12 @@ func (this *FunServantImpl) McGet(ctx *rpc.ReqCtx, key string) (r []byte,
 
 func (this *FunServantImpl) McAdd(ctx *rpc.ReqCtx, key string, value []byte,
 	expiration int32) (r bool, intError error) {
-	err := this.mc.Add(&memcache.Item{Key: key, Value: value,
+	intError = this.mc.Add(&memcache.Item{Key: key, Value: value,
 		Expiration: expiration})
-	if err == nil {
+	if intError == nil {
 		r = true
+	} else {
+		log.Error(intError)
 	}
 
 	return
@@ -48,9 +56,11 @@ func (this *FunServantImpl) McAdd(ctx *rpc.ReqCtx, key string, value []byte,
 
 func (this *FunServantImpl) McDelete(ctx *rpc.ReqCtx, key string) (r bool,
 	intError error) {
-	err := this.mc.Delete(key)
-	if err == nil {
+	intError = this.mc.Delete(key)
+	if intError == nil {
 		r = true
+	} else {
+		log.Error(intError)
 	}
 
 	return
