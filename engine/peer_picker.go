538889e1df72a8ab83647eb05b1042f628b1db1a
funkygao <funky.gao@gmail.com>
2014-02-06 17:58:05 +0800

    peer picker
    consistent hash

---
+++ engine/peer_picker.go
@@ -0,0 +1,44 @@
+package engine
+
+import (
+	"github.com/funkygao/golib/hash"
+	"sync"
+)
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+type PeerPicker interface {
+	PickPeer(key string) (serverAddr string, ok bool)
+}
+
+type consistentPeerPicker struct {
+	self string
+	*sync.Mutex
+	peers *hash.Map
+}
+
+func newPeerPicker(self string) PeerPicker {
+	return &consistentPeerPicker{peers: hash.New(3, nil), Mutex: new(sync.Mutex),
+		self: self}
+}
+
+func (this *consistentPeerPicker) AddPeer(peers ...string) {
+	this.Lock()
+	defer this.Unlock()
+	this.peers.Add(peers...)
+}
+
+func (this *consistentPeerPicker) PickPeer(key string) (serverAddr string, ok bool) {
+	this.Lock()
+	defer this.Unlock()
+	if this.peers.IsEmpty() {
+		return "", false
+	}
+
+	if peer := this.peers.Get(key); peer != this.self {
+		return peer, true
+	}
+
+	return "", false
+
+}
