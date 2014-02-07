da17e319a29c1db076d3d838f8b87ccbfd5e3749
funkygao <funky.gao@gmail.com>
2014-02-07 15:07:59 +0800

    namespace

--- client/php/test.php
+++ client/php/test.php
@@ -6,8 +6,8 @@ require_once $GLOBALS['THRIFT_ROOT'].'/Thrift.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Exception/TException.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Protocol/TBinaryProtocol.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/TStringFunc.php';
-require_once $GLOBALS['THRIFT_ROOT'].'/Factory/TStringFuncFactory.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/Core.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Factory/TStringFuncFactory.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Type/TType.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Type/TMessageType.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TSocket.php';
@@ -16,20 +16,26 @@ require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TBufferedTransport.php';
 require_once $GLOBALS['SERVANT_ROOT'].'/FunServant.php';
 require_once $GLOBALS['SERVANT_ROOT'].'/Types.php';
 
+use Thrift\Transport\TSocketPool;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Protocol\TBinaryProtocol;
+use fun\rpc\FunServantClient;
+use fun\rpc\Context;
+
 try {
-    $sock = new Thrift\Transport\TSocketPool(array('localhost'), array(9001));
+    $sock = new TSocketPool(array('localhost'), array(9001));
     $sock->setDebug(0);
     $sock->setSendTimeout(1000);
     $sock->setRecvTimeout(2500);
     $sock->setNumRetries(1);
-    $transport = new Thrift\Transport\TBufferedTransport($sock, 1024, 1024);
-    $protocol = new Thrift\Protocol\TBinaryProtocol($transport);
+    $transport = new TBufferedTransport($sock, 1024, 1024);
+    $protocol = new TBinaryProtocol($transport);
 
     // get our client
-    $client = new fun\rpc\FunServantClient($protocol);
+    $client = new FunServantClient($protocol);
     $transport->open();
 
-    $ctx = new fun\rpc\Context(array('caller' => "me"));
+    $ctx = new Context(array('caller' => "me"));
     $return = $client->ping($ctx);
     echo $return;
 
