4045133722ade9600a617e4c375da8c77420d85d
funkygao <funky.gao@gmail.com>
2014-02-07 14:48:24 +0800

    php can run now

--- client/php/test.php
+++ client/php/test.php
@@ -1,27 +1,35 @@
 <?php
 
 $GLOBALS['THRIFT_ROOT'] = '/opt/app/thrift/lib/php';
+$GLOBALS['SERVANT_ROOT'] = '../../servant/gen-php/fun/rpc';
 require_once $GLOBALS['THRIFT_ROOT'].'/Thrift.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Exception/TException.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Protocol/TBinaryProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/TStringFunc.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Factory/TStringFuncFactory.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/StringFunc/Core.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Type/TType.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Type/TMessageType.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TSocket.php';
-require_once $GLOBALS['THRIFT_ROOT'].'/Transport/THttpClient.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TSocketPool.php';
 require_once $GLOBALS['THRIFT_ROOT'].'/Transport/TBufferedTransport.php';
-
-$GEN_DIR = '../../servant/gen-php';  
-
-require_once $GEN_DIR . '/fun/rpc/FunServant.php';  
-require_once $GEN_DIR . '/fun/rpc/Types.php';  
+require_once $GLOBALS['SERVANT_ROOT'].'/FunServant.php';
+require_once $GLOBALS['SERVANT_ROOT'].'/Types.php';
 
 try {
-    $socket = new TSocket('localhost', 9001);
-    $transport = new TBufferedTransport($socket, 1024, 1024);
-    $protocol = new TBinaryProtocol($transport);
+    $sock = new Thrift\Transport\TSocketPool(array('localhost'), array(9001));
+    $sock->setDebug(0);
+    $sock->setSendTimeout(1000);
+    $sock->setRecvTimeout(2500);
+    $sock->setNumRetries(1);
+    $transport = new Thrift\Transport\TBufferedTransport($sock, 1024, 1024);
+    $protocol = new Thrift\Protocol\TBinaryProtocol($transport);
 
     // get our client
-    $client = new FunServantClient($protocol);
+    $client = new fun\rpc\FunServantClient($protocol);
     $transport->open();
 
-    $ctx = new req_ctx(array('caller' => "me"));
+    $ctx = new fun\rpc\Context(array('caller' => "me"));
     $return = $client->ping($ctx);
     echo $return;
 
