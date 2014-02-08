ff4faa6c2ac65decc0c472cdd068a270732f9e58
funkygao <funky.gao@gmail.com>
2014-02-08 16:33:56 +0800

    caller info string

--- servant/context.go
+++ servant/context.go
@@ -1,11 +1,14 @@
 package servant
 
 import (
+	"fmt"
 	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
 	"strings"
 )
 
 type callerInfo struct {
+	ctx *rpc.Context
+
 	httpMethod string
 	uri        string
 	seqId      string
@@ -15,6 +18,25 @@ func (this *callerInfo) Valid() bool {
 	return this.seqId != ""
 }
 
+func (this callerInfo) String() string {
+	if !this.Valid() {
+		return "Invalid"
+	}
+
+	s := fmt.Sprintf("%s[%s] %s", this.httpMethod, this.seqId, this.uri)
+	if this.ctx.Host != nil {
+		s = fmt.Sprintf("%s <%s", s, *this.ctx.Host)
+	}
+	if this.ctx.Ip != nil {
+		s = fmt.Sprintf("%s >%s", s, *this.ctx.Ip)
+	}
+	if this.ctx.Sid != nil {
+		s = fmt.Sprintf("%s ^%s", s, *this.ctx.Sid)
+	}
+
+	return s
+}
+
 func (this *FunServantImpl) callerInfo(ctx *rpc.Context) (r callerInfo) {
 	const N = 3
 	p := strings.SplitN(ctx.Caller, "+", N)
@@ -22,8 +44,8 @@ func (this *FunServantImpl) callerInfo(ctx *rpc.Context) (r callerInfo) {
 		return
 	}
 
-	r.httpMethod = p[0]
-	r.uri = p[1]
-	r.seqId = p[2]
+	r.ctx = ctx
+	r.httpMethod, r.uri, r.seqId = p[0], p[1], p[2]
+
 	return
 }
