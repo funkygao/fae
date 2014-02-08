fa1a32f20bf086ab9b5fbf6ef9df1dca7c0e6b1c
funkygao <funky.gao@gmail.com>
2014-02-08 16:21:38 +0800

    install to our project

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ cd ../daemon/faed
 if [[ $1 = "-linux" ]]; then
     # cd $GOROOT/src; CGO_ENABLED=0 GOOS=linux GOARCH=386 ./make.bash
     CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
+    cp -f faed /Users/gaopeng/fun/royalstory-server-code/daemon/
+    exit
 else
     #go build -race -v -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
     go build -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
