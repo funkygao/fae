ab6f73e3ff3440b6639f92f5671d8ec3efddec32
funkygao <funky.gao@gmail.com>
2014-02-11 21:37:23 +0800

    split

--- servant/mg.go
+++ servant/mg.go
@@ -196,29 +196,3 @@ func (this *FunServantImpl) MgFindAndModify(ctx *rpc.Context,
 
 	return
 }
-
-func (this *FunServantImpl) mongoSession(kind string,
-	shardId int32) (*mongo.Session, error) {
-	kind = this.normalizedKind(kind)
-	sess, err := this.mg.Session(kind, shardId)
-	if err != nil {
-		log.Error("{kind^%s id^%d} %s", kind, shardId, err)
-		return nil, err
-	}
-
-	return sess, err
-}
-
-func (this *FunServantImpl) normalizedKind(kind string) string {
-	const (
-		N      = 2
-		PREFIX = "database."
-	)
-
-	p := strings.SplitN(kind, PREFIX, N)
-	if len(p) == 2 {
-		return p[1]
-	}
-
-	return kind
-}
