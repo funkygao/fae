9c17c0be2b86686c8b67871c243f29970fc1003e
funkygao <funky.gao@gmail.com>
2014-02-17 21:11:55 +0800

    mongo pkg util func

---
+++ servant/mongo/util.go
@@ -0,0 +1,31 @@
+package mongo
+
+import (
+	log "github.com/funkygao/log4go"
+	"labix.org/v2/mgo/bson"
+)
+
+// specs: inbound params use json
+func UnmarshalIn(d []byte) (v bson.M, err error) {
+	err = bson.Unmarshal(d, &v)
+	if err != nil {
+		log.Error("unmarshalIn error: %s -> %s", d, err)
+	}
+
+	return
+}
+
+// specs: outbound data use bson
+func MarshalOut(d bson.M) []byte {
+	val, err := bson.Marshal(d)
+	if err != nil {
+		// should never happen
+		log.Critical("marshalOut error: %+v -> %v", d, err)
+	}
+
+	return val
+}
+
+func FieldsIsNil(fields []byte) bool {
+	return len(fields) <= 5
+}
