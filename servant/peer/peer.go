8033fd704193016ac72c37275e2271b899b1e999
funkygao <funky.gao@gmail.com>
2014-02-17 10:11:44 +0800

    refactor

--- servant/peer/peer.go
+++ servant/peer/peer.go
@@ -1,7 +1,6 @@
 package peer
 
 import (
-	"bufio"
 	"encoding/json"
 	"github.com/funkygao/golib/ip"
 	log "github.com/funkygao/log4go"
@@ -101,40 +100,6 @@ func (this *Peer) Start() (err error) {
 	log.Info("Peer[%s] joined at %s", this.selfAddr, this.groupAddr)
 
 	return
-}
-
-func (this *Peer) discoverPeers() {
-	defer func() {
-		this.c.Close() // leave the multicast group
-	}()
-
-	var msg peerMessage
-	reader := bufio.NewReader(this.c)
-	for {
-		// net.ListenMulticastUDP sets IP_MULTICAST_LOOP=0 as
-		// default, so you never receive your own sent data
-		// if you run both sender and receiver on (logically) same IP host
-		line, _, err := reader.ReadLine()
-		if err != nil {
-			log.Error(err)
-			continue
-		}
-
-		if err := msg.unmarshal(line); err != nil {
-			// Not our protocol, it may be SSDP or else
-			continue
-		}
-
-		log.Debug("received peer: %+v", msg)
-
-		neighborIp, present := msg["ip"]
-		if !present {
-			log.Info("Peer msg has no 'ip'")
-			continue
-		}
-
-		this.refreshNeighbor(neighborIp.(string))
-	}
 }
 
 func (this *Peer) publish(msg peerMessage) (err error) {
--- servant/peer/watchdog.go
+++ servant/peer/watchdog.go
@@ -1,6 +1,7 @@
 package peer
 
 import (
+	"bufio"
 	log "github.com/funkygao/log4go"
 	"time"
 )
@@ -24,5 +25,39 @@ func (this *Peer) runHeartbeat() {
 			}
 		}
 
+	}
+}
+
+func (this *Peer) discoverPeers() {
+	defer func() {
+		this.c.Close() // leave the multicast group
+	}()
+
+	var msg peerMessage
+	reader := bufio.NewReader(this.c)
+	for {
+		// net.ListenMulticastUDP sets IP_MULTICAST_LOOP=0 as
+		// default, so you never receive your own sent data
+		// if you run both sender and receiver on (logically) same IP host
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		if err := msg.unmarshal(line); err != nil {
+			// Not our protocol, it may be SSDP or else
+			continue
+		}
+
+		log.Debug("received peer: %+v", msg)
+
+		neighborIp, present := msg["ip"]
+		if !present {
+			log.Info("Peer msg has no 'ip'")
+			continue
+		}
+
+		this.refreshNeighbor(neighborIp.(string))
 	}
 }
