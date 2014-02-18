8ab816ef3e5a98c80349fd64079ff12e7a104931
funkygao <funky.gao@gmail.com>
2014-02-18 10:50:11 +0800

    merge two files

--- servant/mg_helper.go
+++ servant/mg_helper.go
@@ -2,8 +2,17 @@ package servant
 
 import (
 	"github.com/funkygao/fae/servant/mongo"
+	"github.com/funkygao/golib/trace"
 	log "github.com/funkygao/log4go"
 )
+
+type mongoProtocolLogger struct {
+}
+
+func (this *mongoProtocolLogger) Output(calldepth int, s string) error {
+	log.Debug("(%s) %s", trace.CallerFuncName(calldepth), s)
+	return nil
+}
 
 func (this *FunServantImpl) mongoSession(pool string,
 	shardId int32) (*mongo.Session, error) {
