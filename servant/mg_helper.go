c367b94b5f9c0f5f54a91dbec90fa05fcc4db326
funkygao <funky.gao@gmail.com>
2014-02-18 09:15:44 +0800

    fix

--- servant/mg_helper.go
+++ servant/mg_helper.go
@@ -3,7 +3,6 @@ package servant
 import (
 	"github.com/funkygao/fae/servant/mongo"
 	log "github.com/funkygao/log4go"
-	"labix.org/v2/mgo/bson"
 )
 
 func (this *FunServantImpl) mongoSession(pool string,
