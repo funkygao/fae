66bcbdfe034a13b8d93b1984ace6d5b160e063b7
funkygao <funky.gao@gmail.com>
2014-02-21 09:15:28 +0800

    fix compile err

--- servant/svt_dlog.go
+++ servant/svt_dlog.go
@@ -7,14 +7,13 @@ import (
 	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
 	"github.com/funkygao/golib/dlog"
 	log "github.com/funkygao/log4go"
-	"time"
 )
 
 func (this *FunServantImpl) Dlog(ctx *rpc.Context, ident string, tag string,
 	json string) (appErr error) {
 	this.stats.inc("dlog")
 
-	if err := dlog.Dlog(ident, tag, jsonStr); err != nil {
+	if err := dlog.Dlog(ident, tag, json); err != nil {
 		log.Error("dlog: %v", err)
 	}
 
