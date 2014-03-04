3d6f28263ce1ba025144d3d6aceff9a2c69b0ab2
funkygao <funky.gao@gmail.com>
2014-03-04 16:28:41 +0800

    rusage stats

--- daemon/faed/watchdog.go
+++ daemon/faed/watchdog.go
@@ -5,17 +5,38 @@ import (
 	"github.com/funkygao/golib/gofmt"
 	log "github.com/funkygao/log4go"
 	"runtime"
+	"syscall"
 	"time"
 )
 
 func runWatchdog(interval time.Duration) {
-	startTime := time.Now()
-	ms := new(runtime.MemStats)
+
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
+	var (
+		startTime    = time.Now()
+		ms           = new(runtime.MemStats)
+		rusage       = &syscall.Rusage{}
+		lastUserTime int64
+		lastSysTime  int64
+		userTime     int64
+		sysTime      int64
+		userCpuUtil  float64
+		sysCpuUtil   float64
+	)
+
 	for _ = range ticker.C {
 		runtime.ReadMemStats(ms)
+
+		syscall.Getrusage(syscall.RUSAGE_SELF, rusage)
+		userTime = rusage.Utime.Sec*1000000000 + int64(rusage.Utime.Usec)
+		sysTime = rusage.Stime.Sec*1000000000 + int64(rusage.Stime.Usec)
+		userCpuUtil = float64(userTime-lastUserTime) * 100 / float64(interval)
+		sysCpuUtil = float64(sysTime-lastSysTime) * 100 / float64(interval)
+
+		lastUserTime = userTime
+		lastSysTime = sysTime
 
 		log.Info("ver:%s, tick:%ds goroutine:%d, mem:%s, elapsed:%s",
 			engine.BuildID,
@@ -23,5 +44,9 @@ func runWatchdog(interval time.Duration) {
 			runtime.NumGoroutine(),
 			gofmt.ByteSize(ms.Alloc),
 			time.Since(startTime))
+		log.Info("cpu: %3.2f%% us, %3.2f%% sy, rss:%s",
+			userCpuUtil,
+			sysCpuUtil,
+			gofmt.ByteSize(float64(rusage.Maxrss)))
 	}
 }
