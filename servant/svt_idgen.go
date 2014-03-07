c59c259a36315073bd4c5f61b62fa8d603b27432
funkygao <funky.gao@gmail.com>
2014-03-07 12:24:33 +0800

    log debug Idgen

--- servant/svt_idgen.go
+++ servant/svt_idgen.go
@@ -2,12 +2,15 @@ package servant
 
 import (
 	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
+	log "github.com/funkygao/log4go"
 )
 
 // Ticket server
 func (this *FunServantImpl) IdNext(ctx *rpc.Context,
 	flag int16) (r int64, backwards *rpc.TIdTimeBackwards, appErr error) {
 	this.stats.inc("id.next")
+
+	log.Debug("id.next from %+v", *ctx)
 
 	r, appErr = this.idgen.Next()
 	if appErr != nil {
