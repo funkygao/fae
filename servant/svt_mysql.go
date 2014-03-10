02e6f1233a32dc8a63492f582f29a4340176a510
funkygao <funky.gao@gmail.com>
2014-03-10 21:46:38 +0800

    mysql servant skeleton

--- servant/svt_mysql.go
+++ servant/svt_mysql.go
@@ -1,1 +1,24 @@
 package servant
+
+import (
+	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
+	log "github.com/funkygao/log4go"
+)
+
+func (this *FunServantImpl) MyQuery(ctx *rpc.Context, pool string, table string,
+	shardId int32, sql string) (r [][]byte, appErr error) {
+	profiler := this.profiler()
+	profiler.do("my.query", ctx,
+		"{pool^%s table^%s sql^%s} {r^%v}",
+		pool, table, sql, r)
+	return
+}
+
+func (this *FunServantImpl) MyQueryOne(ctx *rpc.Context, pool string, table string,
+	shardId int32, sql string) (r []byte, appErr error) {
+	profiler := this.profiler()
+	profiler.do("my.queryOne", ctx,
+		"{pool^%s table^%s sql^%s} {r^%v}",
+		pool, table, sql, r)
+	return
+}
