44d9cb9529b4b30757c41519af4ae642fe95f9dc
funkygao <funky.gao@gmail.com>
2014-03-11 21:09:43 +0800

    add circuit breaker support

--- servant/mysql/cluster.go
+++ servant/mysql/cluster.go
@@ -2,17 +2,20 @@ package mysql
 
 import (
 	"github.com/funkygao/fae/config"
+	"github.com/funkygao/golib/breaker"
 )
 
 type MysqlCluster struct {
 	conf     *config.ConfigMysql
 	selector ServerSelector
+	breakers map[string]*breaker.Consecutive
 	clients  map[string]*mysql // key is pool name
 }
 
 func New(cf *config.ConfigMysql) *MysqlCluster {
 	this := new(MysqlCluster)
 	this.conf = cf
+	this.breakers = make(map[string]*breaker.Consecutive)
 	switch cf.ShardStrategy {
 	default:
 		this.selector = newStandardServerSelector()
