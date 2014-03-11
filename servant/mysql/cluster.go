82d4ed7164740801a0d3cf5cbc554d22cb5c13b2
funkygao <funky.gao@gmail.com>
2014-03-11 21:44:18 +0800

    let selector manages mysql instances in cluster

--- servant/mysql/cluster.go
+++ servant/mysql/cluster.go
@@ -3,31 +3,17 @@ package mysql
 import (
 	"database/sql"
 	"github.com/funkygao/fae/config"
-	"github.com/funkygao/golib/breaker"
 )
 
 type MysqlCluster struct {
-	conf     *config.ConfigMysql
 	selector ServerSelector
-	breakers map[string]*breaker.Consecutive // key is dsn
-	clients  map[string]*mysql               // key is dsn
 }
 
 func New(cf *config.ConfigMysql) *MysqlCluster {
 	this := new(MysqlCluster)
-	this.conf = cf
-	this.breakers = make(map[string]*breaker.Consecutive)
-
 	switch cf.ShardStrategy {
 	default:
-		this.selector = newStandardServerSelector()
-	}
-	this.selector.SetServers(cf)
-	this.clients = make(map[string]*mysql)
-	for _, server := range cf.Servers {
-		my := newMysql(server.DSN())
-		my.setMaxIdleConns(cf.MaxIdleConnsPerServer)
-		this.clients[server.DSN()] = my
+		this.selector = newStandardServerSelector(cf)
 	}
 
 	return this
--- servant/mysql/selector_standard.go
+++ servant/mysql/selector_standard.go
@@ -2,21 +2,29 @@ package mysql
 
 import (
 	"github.com/funkygao/fae/config"
+	"github.com/funkygao/golib/breaker"
 )
 
 type StandardServerSelector struct {
+	conf     *config.ConfigMysql
+	breakers map[string]*breaker.Consecutive // key is dsn
+	clients  map[string]*mysql               // key is dsn
 }
 
-func newStandardServerSelector() (this *StandardServerSelector) {
+func newStandardServerSelector(cf *config.ConfigMysql) (this *StandardServerSelector) {
 	this = new(StandardServerSelector)
+	this.conf = cf
+	this.breakers = make(map[string]*breaker.Consecutive)
+	this.clients = make(map[string]*mysql)
+	for _, server := range cf.Servers {
+		my := newMysql(server.DSN())
+		my.setMaxIdleConns(cf.MaxIdleConnsPerServer)
+		this.clients[server.DSN()] = my
+	}
 	return
 }
 
-func (this *StandardServerSelector) SetServers(servers *config.ConfigMysql) {
-
-}
-
 func (this *StandardServerSelector) PickServer(pool string,
-	shardId int) (addr string, err error) {
+	shardId int) (my *mysql, err error) {
 	return
 }
