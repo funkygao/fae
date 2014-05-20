ec2df3ca08cada0895b22900ba0b4bdf19770512
funkygao <funky.gao@gmail.com>
2014-05-20 10:36:22 +0800

    release generated code stub

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ cd ../daemon/faed
 
 if [[ $1 = "-linux" ]]; then
     cp -f ../../servant/gen-php/fun/rpc/* /Users/gaopeng/fun/royalstory-server-code/system/fae/
+    cp -f ../../servant/gen-php/fun/rpc/* /Users/gaopeng/fun/dragon-server-code/v2/fae
     # cd $GOROOT/src; CGO_ENABLED=0 GOOS=linux GOARCH=386 ./make.bash
     CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
     cp -f faed /Users/gaopeng/fun/royalstory-server-code/daemon/
