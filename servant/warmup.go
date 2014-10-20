0c4ce1284cd909baa701ab8c5a0f9197df8b5a17
funkygao <funky.gao@gmail.com>
2014-10-20 09:51:18 +0800

    conf file

--- servant/warmup.go
+++ servant/warmup.go
@@ -1,6 +1,14 @@
 package servant
 
 func (this *FunServantImpl) warmUp() {
-	go this.mg.Warmup()
-	go this.mc.Warmup()
+	if this.mg != nil {
+		go this.mg.Warmup()
+	}
+
+	if this.mc != nil {
+		go this.mc.Warmup()
+	}
+
+	// TODO warmup mysql
+
 }
