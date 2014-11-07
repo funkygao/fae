0b75ff1fcced5e2f71698dbbb9d3f008143c5229
funkygao <funky.gao@gmail.com>
2014-11-07 18:42:59 +0800

    build for linux amd64

--- build.sh
+++ build.sh
@@ -13,8 +13,9 @@ cd ../daemon/faed
 if [[ $1 = "-linux" ]]; then
     #cp -f ../../servant/gen-php/fun/rpc/* /Users/gaopeng/fun/royalstory-server-code/system/fae/
     #cp -f ../../servant/gen-php/fun/rpc/* /Users/gaopeng/fun/dragon-server-code/v2/fae
-    #cd $GOROOT/src; CGO_ENABLED=0 GOOS=linux GOARCH=386 ./make.bash
-    CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
+    #cd $GOROOT/src 
+    #sudo CGO_ENABLED=0 GOOS=linux GOARCH=amd64 ./make.bash
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
     exit
 else
     #go build -race -v -ldflags "-X github.com/funkygao/fae/engine.BuildID $ID"
