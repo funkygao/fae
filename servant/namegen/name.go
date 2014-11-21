d053b4241c81ba623f9a314107d698cdc6ecb7cc
funkygao <funky.gao@gmail.com>
2014-11-21 23:53:10 +0800

    bit set/get bitmap is done

--- servant/namegen/name.go
+++ servant/namegen/name.go
@@ -7,8 +7,8 @@ import (
 
 // TODO space will not be used
 const (
-	NameCharMin uint8 = 33
-	NameCharMax uint8 = 126
+	NameCharMin = uint8('!') // 33, space is 32
+	NameCharMax = uint8('~') // 126
 )
 
 func init() {
@@ -19,24 +19,53 @@ type NameGen struct {
 	bits [][]byte
 }
 
-func New(size int) (this *NameGen) {
+func New(slots int) (this *NameGen) {
 	this = new(NameGen)
-	this.bits = make([][]byte, size)
+	this.bits = make([][]byte, slots)
 	for i, _ := range this.bits {
 		this.bits[i] = make([]byte, (NameCharMax-NameCharMin)/8+1)
 
-		for j := NameCharMin; j <= NameCharMax; j++ {
-			this.bits[i][this.pos(j)] = 0
+		for char := NameCharMin; char <= NameCharMax; char++ {
+			this.bits[i][this.pos(char)] = 0x00
 		}
 	}
 
 	return
 }
 
-func (this *NameGen) pos(c uint8) int {
-	var x = c - NameCharMin
+func (this *NameGen) offset(char uint8) uint {
+	return uint(char - NameCharMin)
+}
 
-	return int(x / 8)
+func (this *NameGen) pos(char uint8) uint {
+	return uint(this.offset(char) / 8)
+}
+
+func (this *NameGen) index(char uint8) int {
+	return int(this.offset(char) % 8)
+}
+
+func (this *NameGen) getBit(slot int, char uint8) uint8 {
+	index, pos := this.index(char), this.pos(char)
+	if slot > len(this.bits) || char < NameCharMin || char > NameCharMax {
+		return 0
+	}
+
+	return (this.bits[slot][index] >> pos) & 0x01
+}
+
+func (this *NameGen) setBit(slot int, char uint8, value uint8) {
+	index, pos := this.index(char), this.pos(char)
+	if slot > len(this.bits) || char < NameCharMin || char > NameCharMax {
+		return
+	}
+
+	// value can only be 0/1
+	if value == 0 {
+		this.bits[slot][index] &^= 0x01 << pos
+	} else {
+		this.bits[slot][index] |= 0x01 << pos
+	}
 
 }
 
@@ -44,6 +73,7 @@ func (this *NameGen) Next() string {
 	rv := ""
 	for i := 0; i < len(this.bits); i++ {
 		w := NameCharMin + uint8(rand.Int31n(int32(NameCharMax-NameCharMin)))
+		this.setBit(i, w, 1)
 
 		rv += string(w)
 	}
@@ -51,5 +81,5 @@ func (this *NameGen) Next() string {
 }
 
 func (this *NameGen) Size() int {
-	return len(this.bits[0]) * len(this.bits)
+	return len(this.bits[0]) * len(this.bits) * 8
 }
--- servant/namegen/name_test.go
+++ servant/namegen/name_test.go
@@ -1,19 +1,29 @@
 package namegen
 
 import (
+	"github.com/funkygao/assert"
 	"testing"
 )
 
-func TestAll(t *testing.T) {
-	nm := New(3)
-	t.Logf("%s", nm.Next())
+func TestName3(t *testing.T) {
+	slots := 3
+	nm := New(slots)
+	v1 := nm.Next()
+	v2 := nm.Next()
+	t.Logf("v1:%s, v2:%s, slots:%d, mem usage: %d", v1, v2, slots, nm.Size())
+	t.Logf("%+v", nm.bits)
+	for i := 0; i < 1000; i++ {
+		t.Logf("%s", nm.Next())
+	}
+	assert.NotEqual(t, v1, v2)
+
 }
 
+// 529 ns/op
 func BenchmarkNextName(b *testing.B) {
 	b.ReportAllocs()
-	nm := New(4)
-	b.Logf("mem space usage: %d", nm.Size())
+	nm := New(3)
 	for i := 0; i < b.N; i++ {
-		b.Logf("%s", nm.Next())
+		nm.Next()
 	}
 }
