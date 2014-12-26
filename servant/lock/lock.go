39867b4c22b68701854c0a8d9f036ac7e7eab20d
funkygao <funky.gao@gmail.com>
2014-12-26 12:30:44 +0800

    add missing files

---
+++ servant/lock/lock.go
@@ -0,0 +1,50 @@
+package lock
+
+import (
+	"github.com/funkygao/fae/config"
+	"github.com/funkygao/golib/cache"
+	log "github.com/funkygao/log4go"
+	"sync"
+	"time"
+)
+
+type Lock struct {
+	cf *config.ConfigLock
+
+	items *cache.LruCache // key: mtime
+	mutex sync.Mutex      // lru get/set is safe, but we need more lock span
+}
+
+func New(cf *config.ConfigLock) *Lock {
+	this := &Lock{cf: cf}
+	this.items = cache.NewLruCache(cf.MaxItems)
+	return this
+}
+
+func (this *Lock) Lock(key string) (success bool) {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
+
+	mtime, present := this.items.Get(key)
+	if !present {
+		this.items.Set(key, time.Now())
+
+		return true
+	}
+
+	// present, check expires
+	elapsed := time.Since(mtime.(time.Time))
+	if this.cf.Expires > 0 && elapsed > this.cf.Expires {
+		log.Warn("lock[%s] expires: %s, kicked", key, elapsed)
+
+		// ignore the aged lock, refresh the lock
+		this.items.Set(key, time.Now())
+		return true
+	}
+
+	return false
+}
+
+func (this *Lock) Unlock(key string) {
+	this.items.Del(key)
+}
