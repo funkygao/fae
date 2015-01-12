a6b45ea5ee096c11824044a44657e7d70aa03a54
funkygao <funky.gao@gmail.com>
2015-01-12 22:02:45 +0800

    rm outdated comment

--- servant/session.go
+++ servant/session.go
@@ -42,7 +42,7 @@ func (this *session) startProfiler() (*profiler, error) {
 
 		this.profiler = &profiler{}
 		// TODO 某些web server需要100%采样
-		this.profiler.on = sampling.SampleRateSatisfied(config.Engine.Servants.ProfilerRate) // rand(1000) <= ProfilerRate
+		this.profiler.on = sampling.SampleRateSatisfied(config.Engine.Servants.ProfilerRate)
 		this.profiler.t0 = time.Now()
 		this.profiler.t1 = this.profiler.t0
 	}
