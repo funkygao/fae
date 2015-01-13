e46b1c74ee79e7de187aca3b934b9793297b01cf
funkygao <funky.gao@gmail.com>
2015-01-13 18:12:22 +0800

    game section is required

--- config/servant.go
+++ config/servant.go
@@ -70,6 +70,8 @@ func (this *ConfigServant) LoadConfig(selfAddr string, cf *conf.Conf) {
 	section, err = cf.Section("game")
 	if err == nil {
 		this.Game.LoadConfig(section)
+	} else {
+		panic("empty game section")
 	}
 
 	// couchbase section
