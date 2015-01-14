487561271e72ece3ca71419c0abb554426df25ef
funkygao <funky.gao@gmail.com>
2015-01-14 16:02:58 +0800

    run all related tests in a session

--- client/go/stress/session.go
+++ client/go/stress/session.go
@@ -38,40 +38,25 @@ func runSession(proxy *proxy.Proxy, wg *sync.WaitGroup, round int, seq int) {
 			round, seq, time.Since(t1))
 	}
 
-	var (
-		key     string
-		value   []byte
-		mcValue = rpc.NewTMemcacheData()
-		result  []byte
-	)
-	mgQuery, _ := bson.Marshal(bson.M{"snsid": "100003391571259"})
-	mgFields, _ := bson.Marshal(bson.M{})
 	for i := 0; i < LoopsPerSession; i++ {
 		if Cmd&CallPing != 0 {
-			_, err = client.Ping(ctx)
+			var r string
+			r, err = client.Ping(ctx)
 			if err != nil {
 				report.incCallErr()
-				log.Printf("session{round^%d seq^%d ping} %v", round, seq, err)
+				log.Printf("session{round^%d seq^%d ping}: %v", round, seq, err)
 			} else {
 				report.incCallOk()
+				if enableLog {
+					log.Printf("session{round^%d seq^%d ping}: %v", round, seq, r)
+				}
+
 			}
 		}
-
-		if Cmd&CallIdGen != 0 {
-			_, _, err = client.IdNext(ctx)
-			if err != nil {
-				report.incCallErr()
-				log.Printf("session{round^%d seq^%d idgen} %v", round, seq, err)
-			} else {
-				report.incCallOk()
-			}
-		}
-
-		key = fmt.Sprintf("mc_stress:%d", rand.Int())
-		value = []byte("value of " + key)
-		mcValue.Data = value
 
 		if Cmd&CallLCache != 0 {
+			key := fmt.Sprintf("lc_stress:%d", rand.Int())
+			value := []byte("value of " + key)
 			_, err = client.LcSet(ctx, key, value)
 			if err != nil {
 				report.incCallErr()
@@ -79,14 +64,109 @@ func runSession(proxy *proxy.Proxy, wg *sync.WaitGroup, round int, seq int) {
 			} else {
 				report.incCallOk()
 			}
-			_, _, err = client.LcGet(ctx, key)
+
+			value, _, err = client.LcGet(ctx, key)
 			if err != nil {
 				report.incCallErr()
 				log.Printf("session{round^%d seq^%d lc_get} %v", round, seq, err)
 			} else {
 				report.incCallOk()
+				if enableLog {
+					log.Printf("session{round^%d seq^%d lcache}: %s => %s",
+						round, seq, key, string(value))
+				}
 			}
 		}
+
+		if Cmd&CallIdGen != 0 {
+			var r int64
+			r, _, err = client.IdNext(ctx)
+			if err != nil {
+				report.incCallErr()
+				log.Printf("session{round^%d seq^%d idgen}: %v", round, seq, err)
+			} else {
+				if enableLog {
+					log.Printf("session{round^%d seq^%d idgen}: %d",
+						round, seq, r)
+				}
+				report.incCallOk()
+			}
+		}
+
+		if Cmd&CallGame != 0 {
+			client.GmLatency(ctx, 12, 4545)
+			report.incCallOk()
+			if enableLog {
+				log.Printf("session{round^%d seq^%d GmLatency}",
+					round, seq)
+			}
+
+			r, err := client.GmName3(ctx)
+			if err != nil {
+				report.incCallErr()
+				log.Printf("session{round^%d seq^%d GmName3}: %v", round, seq, err)
+			} else {
+				if enableLog {
+					log.Printf("session{round^%d seq^%d GmName3}: %s",
+						round, seq, r)
+				}
+				report.incCallOk()
+			}
+
+			lockKey := fmt.Sprintf("key:%d:%d", round, seq)
+			client.GmLock(ctx, "stress.go", lockKey)
+			report.incCallOk()
+			if enableLog {
+				log.Printf("session{round^%d seq^%d GmLock}: %s",
+					round, seq, lockKey)
+			}
+			client.GmUnlock(ctx, "stress.go", lockKey)
+			report.incCallOk()
+			if enableLog {
+				log.Printf("session{round^%d seq^%d GmUnlock}: %s",
+					round, seq, lockKey)
+			}
+
+			shardId, err := client.GmRegister(ctx, "u")
+			if err != nil {
+				report.incCallErr()
+				log.Printf("session{round^%d seq^%d GmRegister}: %v", round, seq, err)
+			} else {
+				if enableLog {
+					log.Printf("session{round^%d seq^%d GmRegister}: %d",
+						round, seq, shardId)
+				}
+				report.incCallOk()
+			}
+		}
+
+		if Cmd&CallMysql != 0 {
+			r, err := client.MyQuery(ctx, "UserShard", "UserInfo", 2,
+				"SELECT * FROM UserInfo WHERE uid=?",
+				[]string{"1"}, "user:1")
+			if err != nil {
+				report.incCallErr()
+				log.Printf("session{round^%d seq^%d mysql}: %v", round, seq, err)
+			} else {
+				if enableLog {
+					log.Printf("session{round^%d seq^%d mysql}: %+v",
+						round, seq, r)
+				}
+				report.incCallOk()
+			}
+		}
+
+		continue // TODO
+
+		var (
+			key     string
+			value   []byte
+			mcValue = rpc.NewTMemcacheData()
+			result  []byte
+		)
+		key = fmt.Sprintf("mc_stress:%d", rand.Int())
+		value = []byte("value of " + key)
+		mcValue.Data = value
 
 		if Cmd&CallMemcache != 0 {
 			_, err = client.McSet(ctx, MC_POOL, key, mcValue, 36000)
@@ -107,6 +187,8 @@ func runSession(proxy *proxy.Proxy, wg *sync.WaitGroup, round int, seq int) {
 		}
 
 		if Cmd&CallMongo != 0 {
+			mgQuery, _ := bson.Marshal(bson.M{"snsid": "100003391571259"})
+			mgFields, _ := bson.Marshal(bson.M{})
 			result, _, err = client.MgFindOne(ctx, "default", "idmap",
 				0, mgQuery, mgFields)
 			if err != nil {
