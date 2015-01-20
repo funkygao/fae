456c0c65a8b9f3f51f8c38c06fa6f7eb7e641a93
funkygao <funky.gao@gmail.com>
2015-01-20 09:39:12 +0800

    test broken fae conn

---
+++ client/go/broken/broken.go
@@ -0,0 +1,61 @@
+// test a broken fae connection
+// HOW to play:
+// run broken while faed is on, turn off fae, turn on fae
+// see output of broken
+// TODO try zk cluster
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/funkygao/fae/config"
+	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
+	"github.com/funkygao/fae/servant/proxy"
+	log "github.com/funkygao/log4go"
+	"time"
+)
+
+var (
+	host     string
+	port     string
+	interval int
+)
+
+func init() {
+	flag.StringVar(&host, "host", "localhost", "host name of faed")
+	flag.StringVar(&port, "port", "9001", "fae port")
+	flag.IntVar(&interval, "interval", 5, "sleep between conn")
+	flag.Parse()
+
+	log.AddFilter("stdout", log.INFO, log.NewConsoleLogWriter())
+}
+
+func main() {
+	cf := config.NewDefaultProxy()
+	cf.PoolCapacity = 5
+	proxy := proxy.New(cf)
+
+	for {
+		client, err := proxy.Servant(host + ":" + port)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		ctx := rpc.NewContext()
+		ctx.Reason = "broken.test"
+		ctx.Rid = fmt.Sprintf("req:%d", time.Now().UnixNano())
+		pong, err := client.Ping(ctx)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(pong)
+		}
+		fmt.Println()
+
+		client.Recycle()
+
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
+
+}
