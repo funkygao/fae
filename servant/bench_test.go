0687ea268cf4ce619754772c579360f605ca6d60
funkygao <funky.gao@gmail.com>
2015-02-11 14:32:27 +0800

    proof that string and byte slice conversion is costly

--- servant/bench_test.go
+++ servant/bench_test.go
@@ -22,6 +22,25 @@ func setupServant() *FunServantImpl {
 	return NewFunServant(config.Engine.Servants)
 }
 
+// 1327 ns/op
+func BenchmarkByteSliceConvertString(b *testing.B) {
+	b.ReportAllocs()
+	s := strings.Repeat("h", 1000)
+	for i := 0; i < b.N; i++ {
+		_ = []byte(s) // will lead to mem copy and alloc
+	}
+}
+
+// 1338 ns/op
+func BenchmarkStringConvertByteSlice(b *testing.B) {
+	const N = 1000
+	b.ReportAllocs()
+	ba := make([]byte, N) // lower N will show better performance
+	for i := 0; i < b.N; i++ {
+		_ = string(ba) // its costly
+	}
+}
+
 // 683 ns/op
 func BenchmarkSprintfSql(b *testing.B) {
 	b.ReportAllocs()
