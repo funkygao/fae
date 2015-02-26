a5d65c02cb589e35a0997b6f100e706751f94c5f
funkygao <funky.gao@gmail.com>
2015-02-26 08:44:51 +0800

    place benchmark test into a dedicated file

--- servant/store/all_test.go
+++ servant/store/all_test.go
@@ -43,28 +43,3 @@ func runStoreTest(t *testing.T, s Store) {
 	assert.Equal(t, false, present)
 	assert.Equal(t, nil, val)
 }
-
-func BenchmarkRedisStoreSet(b *testing.B) {
-	b.ReportAllocs()
-
-	s := getRedisStore()
-	k, v := "hello_benchmark", "world_benchmark"
-	for i := 0; i < b.N; i++ {
-		s.Set(k, v)
-	}
-
-	b.SetBytes(int64(len(k + v)))
-}
-
-func BenchmarkRedisStoreGet(b *testing.B) {
-	b.ReportAllocs()
-
-	s := getRedisStore()
-	k, v := "hello_benchmark", "world_benchmark"
-	s.Set(k, v)
-	for i := 0; i < b.N; i++ {
-		s.Get(k)
-	}
-
-	b.SetBytes(int64(len(k + v)))
-}
