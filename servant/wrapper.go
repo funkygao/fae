74bd99ee1caf62e509cfaef253fdfbae619ae1ef
funkygao <funky.gao@gmail.com>
2015-03-01 16:57:12 +0800

    wrapper the servant for decorator pattern

---
+++ servant/wrapper.go
@@ -0,0 +1,20 @@
+package servant
+
+import (
+	"github.com/funkygao/fae/config"
+	"github.com/funkygao/fae/servant/gen-go/fun/rpc"
+)
+
+type FunServantImplWrapper struct {
+	*FunServantImpl
+}
+
+func NewFunServantWrapper(cf *config.ConfigServant) (this *FunServantImplWrapper) {
+	this = &FunServantImplWrapper{FunServantImpl: NewFunServant(cf)}
+	return
+}
+
+func (this *FunServantImplWrapper) Ping(ctx *rpc.Context) (r string, ex error) {
+	r, ex = this.FunServantImpl.Ping(ctx)
+	return
+}
